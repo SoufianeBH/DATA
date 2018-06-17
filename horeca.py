dad77188b65fbe3dd46492c9c99a43ac4b5373ed
Soufiane Ben Haddou <soufiane_atom@hotmail.com>
2018-06-17 16:33:15 +0200

    simple map

--- horeca.py
+++ horeca.py
@@ -29,5 +29,19 @@ for x in data["LOCATIE_WKT"]:
 d = {'long': long, 'lat': lat}
 df = pd.DataFrame(data=d)
 
-df.to_csv("horecacoor.csv",index=False)
+df.to_csv("horeca.csv",index=False)
 
+data = pd.read_csv("horeca.csv")
+
+fig = plt.figure()
+
+ax = fig.add_subplot(111)
+
+ax.plot(data["long"], data["lat"], 'k.', markersize=0.7)
+
+x0, x1 = ax.get_xlim()
+y0, y1 = ax.get_ylim()
+img = plt.imread("kaart.png")
+ax.imshow(img, extent = [x0, x1, y0, y1], aspect = "auto", zorder = 0)
+
+plt.savefig("horeca.png")
